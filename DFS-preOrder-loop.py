52830c913d8d9904d919d99e0ed06ac6ba6fc729
amir.gholami <amir.gholami@vodafoneziggo.com>
2025-01-28 17:29:21 +0100

    feat: state definition to keep track of traverse/DFS preorder loop

--- DFS-preOrder-loop.py
+++ DFS-preOrder-loop.py
@@ -89,3 +89,31 @@ class Stack():
 
         else:
             return "<stack is empty>"
+
+class State(object):
+  def __init__(self, node):
+    self.node = node
+    self.visited_left = False
+    self.visited_right = False
+
+  def get_node(self):
+    return self.node
+    
+  def get_visited_left(self):
+    return self.visited_left
+
+  def get_visited_right(self):
+    return self.visited_right
+
+  def set_visited_right(self):
+    self.visited_right = True
+
+  def set_visited_left(self):
+    self.visited_left = True
+
+  def __repr__(self) -> str:
+    s = f"""{self.node}
+visited_left: {self.visited_left}
+visited_right: {self.visited_right}
+        """
+    return s
