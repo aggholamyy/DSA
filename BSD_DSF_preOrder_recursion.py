46ac3c1d0f9787a3ec89bce5e3a1cd9ecf3301c8
amir.gholami <amir.gholami@vodafoneziggo.com>
2025-02-02 14:20:45 +0100

    feat: final correct traversing BST with recursive approach

--- BSD_DSF_preOrder_recursion.py
+++ BSD_DSF_preOrder_recursion.py
@@ -1,9 +1,5 @@
 """
-The following function is not the correct solution for traversing a BST,
-but it is the first recursive function I wrote that handled a specific test.
-The main problem here is that this code traverses the whole tree using two separate recursive calls,
-which causes it to miss many right children.
-I want to keep this to track the evolution process of my solution.
+The following function is the correct solution for traversing a BST,
 """
 
 def travers_preorder_bsd(root):
@@ -12,10 +8,9 @@ def travers_preorder_bsd(root):
   def traveler(node, visited_list):
     if node is None:
       return visited_list
-    current_node = node
-    visited_list.append(current_node)
+    visited_list.append(node)
     traveler(node.get_left_child(), visited_list)
-    traveler(current_node.get_right_child(), visited_list)
+    traveler(node.get_right_child(), visited_list)
     return visited_list
   traveler(root, visited_list)
   return visited_list
