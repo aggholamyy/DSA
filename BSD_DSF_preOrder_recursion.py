8518b03bcea8d7782243237e59a5b23bfabfdb1f
amir.gholami <amir.gholami@vodafoneziggo.com>
2025-02-02 14:09:08 +0100

    feat: leave handeling None nodes to first if instead on checking inside else

--- BSD_DSF_preOrder_recursion.py
+++ BSD_DSF_preOrder_recursion.py
@@ -6,25 +6,22 @@ which causes it to miss many right children.
 I want to keep this to track the evolution process of my solution.
 """
 
-def pre_order(tree):
+def travers_preorder_bsd(node):
   visited_list = list()
 #########################
 # visited_list must be init by root
-  visited_list.append(node)
+#  visited_list.append(node)
   #########################
 
   def traveler(node, visited_list):
     if node is None:
       return visited_list
     current_node = node
+    visited_list.append(current_node)
     node = node.get_left_child()
-    if node:
-      visited_list.append(node)
-    visited_list = traveler(node, visited_list)
+    traveler(node, visited_list)
     node = current_node.get_right_child()
-    if node:
-      visited_list.append(node)
-    visited_list = traveler(node, visited_list)
+    traveler(node, visited_list)
     return visited_list
   traveler(root, visited_list)
   return visited_list
